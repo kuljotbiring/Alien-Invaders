125f25ca1d9c621cab320175d97b832296f2b66f
Kuljot Biring <ksbiring1@gmail.com>
2020-12-15 10:18:33 -0800

    create alien fleet

--- alien_invaders.py
+++ alien_invaders.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 from settings import Settings
 
@@ -27,6 +28,9 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
 
         # Set the background color.
         self.bg_color = (230, 230, 230)
@@ -95,6 +99,22 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+    def _create_fleet(self):
+        """Create the fleet of alients"""
+        # Create analien and fin the number of aliens in a row.
+        # Spacing between each alien is equal to one alien width
+        alien = Alien(self)
+        alien_width = alien.rect.width
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+
+        # Create the first row of aliens.
+        for alien_number in range(number_aliens_x):
+            alien = Alien(self)
+            alien.x = alien_width + 2 * alien_width * alien_number
+            alien.rect.x = alien.x
+            self.aliens.add(alien)
+
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
@@ -102,6 +122,7 @@ class AlienInvasion:
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
 
         pygame.display.flip()
 
